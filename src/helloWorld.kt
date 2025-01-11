bdd28d8159cfe92267df06c5f3445e7a165553cf
Irvin Silva <103979055+Webdeveloperintraining@users.noreply.github.com>
2025-01-11 13:37:28 -0800

    Create helloWorld.kt

---
+++ src/helloWorld.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    println("Hello World!")
+
+  // alternative way to print Hello World!
+    //val greeting = "Hello"
+    //val everyone = "World"
+    //println("$greeting $everyone!")
+
+}
